47b2752abf9d78b638bb16f0abac451e2a447743
Serghei Filippov <blastowind@gmail.com>
2013-08-04 21:04:31 -0400

    Fixed the initialize for the ship

--- ship.py
+++ ship.py
@@ -7,24 +7,26 @@ class Ship(pygame.sprite.Sprite):
     ROT_LEFT = 2
 
     def __init__(self):
+        super(Ship, self).__init__()
+
         self.rect = Ship.IMAGE.get_rect()
-    
+
         self.image = pygame.transform.smoothscale(Ship.IMAGE, (self.rect.width / 4, self.rect.height / 4))
 
         self.rect = self.image.get_rect()
         self.rect.centerx = 50
         self.rect.centery = 50
-        
+
         self._direction = 0
-        
+
     def move():
         '''
 
         '''
-        
+
     def rotate(self, d, angle):
         '''
-        
+
         '''
         if d == ROT_RIGHT:
             IMAGE = pygame.transform.rotate(IMAGE, - angle)
